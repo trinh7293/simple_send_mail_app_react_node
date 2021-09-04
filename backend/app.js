0a9ef1cb8145e660bf226c492bf0df77795142a4
Trinh Hoang <trinh7293@gmail.com>
2021-09-05 01:48:01 +0700

    complete most feature

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,19 @@
+// import dependencies
+const express = require('express')
+const bodyParser = require('body-parser')
 const nodemailer = require('nodemailer')
+
+// create env variable
 const dotenv = require('dotenv')
 dotenv.config()
 
+// init express
+const app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+const port = process.env.PORT || 5000
+
+// init node mailer
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
@@ -11,14 +23,31 @@ const transporter = nodemailer.createTransport({
     }
 });
 
-async function run() {
-    const result = await transporter.sendMail({
-        from: 'from_address@example.com',
-        to: 'trinh7293@gmail.com',
-        subject: 'Test Email Subject',
-        text: 'Example Plain Text Message Body'
-    })
-    console.log(result)
-}
+app.get('/api', (req, res) => {
+    res.send('Hello World!')
+})
 
-run()
+app.post('/post-test', async (req, res) => {
+    console.log('Got body:', req.body);
+    const {
+        from,
+        to,
+        subject,
+        text
+    } = req.body
+    try {
+        const result = await transporter.sendMail({
+            from,
+            to,
+            subject,
+            text
+        })
+        res.send("Send email Successfully")
+    } catch (error) {
+        throw new Error(error)
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
--- frontend/src/services/sendMailService.js
+++ frontend/src/services/sendMailService.js
@@ -1,3 +1,20 @@
-export function sendMailApi(data) {
-    console.log('data', data)
+import axios from "axios";
+
+export async function sendMailApi(data) {
+    const {
+        senderName,
+        senderEmail
+    } = data
+    const from = senderName ? `${senderName} <${senderEmail}>` : senderEmail
+    const {
+        recipientEmail: to,
+        subject: subject,
+        message: text
+    } = data
+    const result = await axios.post('post-test', {
+        from,
+        to,
+        subject,
+        text
+    })
 }
